dde5d3d53b4aae75a2453432ba81f85d8def3799
Blake Lundquist <lundqub@outlook.com>
2020-01-07 12:33:42 -0800

    create basic error boundary example

--- src/ErrorBoundaryExample/ErrorBoundaryExample.js
+++ src/ErrorBoundaryExample/ErrorBoundaryExample.js
@@ -6,10 +6,12 @@ import ErrorPage from '../ErrorPage/ErrorPage'
 class ErrorBoundaryExample extends React.Component {
     render() {
         return (
-            <div className="error-example-page">
-                <h2>This page will brake when I click the button</h2>
-                <BrokenComponent />
-            </div>
+            <ErrorPage>
+              <div className="error-example-page">
+                  <h2>This page will brake when I click the button</h2>
+                  <BrokenComponent />
+              </div>
+            </ErrorPage>
         );
     }
 }
--- src/ErrorPage/ErrorPage.js
+++ src/ErrorPage/ErrorPage.js
@@ -5,10 +5,22 @@ class ErrorPage extends React.Component {
 
     // Static method
     static getDerivedStateFromError(error) {
+        // Called when an error is thrown in a child component
+        console.error(error);
+        // Store the error in the state
+        return {error:true};
     }
 
     render() {
-        
+        if (this.state.error) {
+            return (
+                <h1>Oh No! The application is broken.</h1>
+            )
+        }
+
+        // Otherwise, render the children
+        return this.props.children;
+
     }
 }
 
