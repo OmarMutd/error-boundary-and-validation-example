e1191b5dd9ea9aa10f4a90682f054c8faf594e45
Blake Lundquist <lundqub@outlook.com>
2020-01-07 16:41:47 -0800

    add example code

--- src/ErrorBoundaryExample/ErrorBoundaryExample.js
+++ src/ErrorBoundaryExample/ErrorBoundaryExample.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import BrokenComponent from '../BrokenComponent/BrokenComponent'
-import ErrorPage from '../ErrorPage/ErrorPage'
+import ErrorPage from  '../ErrorPage/ErrorPage'
 
 
 class ErrorBoundaryExample extends React.Component {
     render() {
         return (
-            <div className="error-example-page">
-              <ErrorPage>
-                <h2>This page will brake when I click the button</h2>
-                <BrokenComponent />
-              </ErrorPage>
-            </div>
+            <ErrorPage>
+              <div className="error-example-page">
+                  <h2>This page will brake when I click the button</h2>
+                  <BrokenComponent />
+              </div>
+            </ErrorPage>
         );
     }
 }
--- src/RegistrationForm/RegistrationForm.js
+++ src/RegistrationForm/RegistrationForm.js
@@ -4,46 +4,67 @@ class RegistrationForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      ssn: {
+        value: '',
+        touched: false
+      }
     };
   }
 
-  updateSsn(ssn) {
-    
+  setSsn(ssnInput) {
+    this.setState({
+      ssn: {
+        value: ssnInput,
+        touched: true        
+      }
+    })    
   }
 
-  validateSsn(ssn) {
+  generateErrorMessage = () => {
+    let ssn = this.state.ssn.value;
+    ssn = ssn.replace(/[\s-]/g, ''); // Remove whitespace and dashes
+
+    if (ssn.length !== 9 && this.state.ssn.touched) {
+        // Check if it's 9 characters long
+        return 'SSN must be 9 digits long';
+    } else if (!/^\d+$/.test(ssn) && this.state.ssn.touched) {
+        // Check if it's just digits
+        return 'SSN must only contain numbers';
+    }
 
   }
 
   handleSubmit(event) {
-
+    event.preventDefault();
+    console.log('submit')
   }
 
   render() {
 
     return (
 
-        <form className="registration" onSubmit={e => this.handleSubmit(e)}>
+        <form className="registration" onSubmit={(event) => {this.handleSubmit(event)}}>
           <h2>Register</h2>
-          <div className="registration__hint">* required field</div>
           <div className="form-group">
-            <label htmlFor="ssn">SSN *</label>
+            <label htmlFor="ssn">SSN*</label>
             <input
               type="text"
               className="registration__control"
               name="ssn"
               id="ssn"
+              value={this.state.ssn.value}
+              onChange={(event)=> {this.setSsn(event.target.value)}}
             />
           </div>
-
           <div className="registration__button__group">
             <button
               type="submit"
               className="registration__button"
+              disabled={this.generateErrorMessage()}
             >
               Save
             </button>
+            {this.generateErrorMessage() ? <p>{this.generateErrorMessage()}</p> : ''}
           </div>
         </form>
     );
